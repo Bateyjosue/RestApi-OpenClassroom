b97d22ce60a1f165ad50a9b60114a9efd1cd64a2
Bateyjosue <josuebatey19@gmail.com>
2021-12-11 12:44:32 +0200

    serialize the model Category

---
+++ shop/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Category
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
