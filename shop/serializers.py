a758c923cd03c08e68a061cbd6e088fab1e03b19
Bateyjosue <josuebatey19@gmail.com>
2021-12-16 19:06:11 +0200

    add category serializer

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework.serializers import ModelSerializer
-from shop.models import Category
+from shop.models import Category, Product
 
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'description','active', 'date_updated', 'date_created']
+
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'date_created', 'date_updated', 'name', 'category' ]
