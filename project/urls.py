5f9e97c5a867c4dd818bb16607c8c49251f7573d
Bateyjosue <josuebatey19@gmail.com>
2021-12-17 14:18:03 +0200

    update views

--- project/urls.py
+++ project/urls.py
@@ -5,14 +5,14 @@ from rest_framework import routers
 
 from shop.views import CategoryViewset
 
-# from shop.views import CategoryAPIView, ProductAPIView
+from shop.views import CategoryAPIView, ProductAPIView
 router = routers.SimpleRouter()
 router.register('category', CategoryViewset, basename='category')
+router.register('product', ProductAPIView, basename='product')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
     # path('api/category/', CategoryAPIView.as_view()),
-    # path('api/product/', ProductAPIView.as_view()),
 ]
